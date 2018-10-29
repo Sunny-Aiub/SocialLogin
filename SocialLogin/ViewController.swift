8ecb9b9879afcada7f9f9dcf978ae43e28be8b5d
Md. Mahadhi Hassan Chowdhury <Sunny@dream71s-imac.local>
2018-10-29 10:32:50 +0600

    first commit

---
+++ SocialLogin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SocialLogin
+//
+//  Created by Md. Mahadhi Hassan Chowdhury on 10/29/18.
+//  Copyright © 2018 Md. Mahadhi Hassan Chowdhury. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
